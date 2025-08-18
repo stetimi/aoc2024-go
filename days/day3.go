fd99f3f67cc7ad6956dd861ea4132b3b91964108
stetimi <123815784+stetimi@users.noreply.github.com>
2025-08-18 09:47:26 +0800

    Day 3, part 2

--- days/day3.go
+++ days/day3.go
@@ -6,28 +6,45 @@ import (
 	"strconv"
 )
 
+const MUL_REGEXP = `mul\(([0-9]+),([0-9]+)\)`
+
 func Day3() {
 	program := utils.MustReadFile("resources/day3.txt")
-	println("Part 1: ", runProgram(program))
+	println("Part 1: ", runDay1Program(program))
+	println("Part 2: ", runDay2Program(program))
 }
 
-func runProgram(memory []byte) int {
-	re := mulRegexp()
+func runDay1Program(memory []byte) int {
+	return runProgram(memory, regexp.MustCompile(MUL_REGEXP))
+}
+
+func runDay2Program(memory []byte) int {
+	return runProgram(memory, regexp.MustCompile(`do\(\)|don't\(\)|`+MUL_REGEXP))
+}
+
+func runProgram(memory []byte, re *regexp.Regexp) int {
 	matches := re.FindAllSubmatch(memory, -1)
 	sum := 0
+	enabled := true
 	for _, match := range matches {
+		switch string(match[0]) {
+		case "do()":
+			enabled = true
+			continue
+		case "don't()":
+			enabled = false
+			continue
+		}
 		if len(match) != 3 {
 			panic("Bad multiplication format")
 		}
-		x := atoi(match[1])
-		y := atoi(match[2])
-		sum += x * y
+		if enabled {
+			x := atoi(match[1])
+			y := atoi(match[2])
+			sum += x * y
+		}
 	}
 	return sum
-}
-
-func mulRegexp() *regexp.Regexp {
-	return regexp.MustCompile(`mul\(([0-9]+),([0-9]+)\)`)
 }
 
 func atoi(b []byte) int {
--- days/day3_test.go
+++ days/day3_test.go
@@ -6,7 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRunProgram(t *testing.T) {
+func TestRunDay1Program(t *testing.T) {
 	memory := []byte("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
-	assert.Equal(t, 161, runProgram(memory))
+	assert.Equal(t, 161, runDay1Program(memory))
+}
+
+func TestRunDay2Program_CustomString(t *testing.T) {
+	memory := []byte("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+	assert.Equal(t, 48, runDay2Program(memory))
 }
