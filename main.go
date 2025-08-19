742174545de96ecad9201167c6141a40a3af26d2
stetimi <123815784+stetimi@users.noreply.github.com>
2025-08-19 19:22:15 +0800

    Update main.go to run multiple days

--- main.go
+++ main.go
@@ -5,22 +5,57 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run main.go <day_number>")
-		os.Exit(1)
-	}
 	dayFuncs := []func(){
 		days.Day1,
 		days.Day2,
 		days.Day3,
 	}
-	day, err := strconv.Atoi(os.Args[1])
-	if err != nil || day < 1 || day > len(dayFuncs) {
-		fmt.Printf("Invalid day number. Please provide an integer between 1 and %d\n", len(dayFuncs))
+	if len(os.Args) == 1 {
+		fmt.Println("No arguments provided. Running all days...")
+		for i, f := range dayFuncs {
+			start := time.Now()
+			f()
+			elapsed := time.Since(start).Seconds() * 1000
+			fmt.Printf("Day %d [%.2f ms]\n", i+1, elapsed)
+		}
+		return
+	}
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <day_number>[,<day_number>...]")
 		os.Exit(1)
 	}
-	dayFuncs[day-1]()
+	daysList := parseDayArgs(os.Args[1], len(dayFuncs))
+	for _, day := range daysList {
+		start := time.Now()
+		dayFuncs[day-1]()
+		elapsed := time.Since(start).Seconds() * 1000
+		fmt.Printf("Day %d [%.2f ms]\n", day, elapsed)
+	}
+}
+
+func parseDayArgs(arg string, maxDay int) []int {
+	daysList := []int{}
+	for _, part := range strings.Split(arg, ",") {
+		trimmed := strings.TrimSpace(part)
+		if trimmed == "" {
+			continue
+		}
+		day, err := strconv.Atoi(trimmed)
+		if err != nil {
+			panic(fmt.Sprintf("Invalid integer: %s", trimmed))
+		}
+		if day < 1 || day > maxDay {
+			panic(fmt.Sprintf("Day out of range: %d. Please provide integers between 1 and %d", day, maxDay))
+		}
+		daysList = append(daysList, day)
+	}
+	if len(daysList) == 0 {
+		panic("No valid day numbers provided.")
+	}
+	return daysList
 }
